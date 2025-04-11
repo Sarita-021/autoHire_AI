9715373b37f5331840119f51c7bb2cbbceae7eb9
Sarita <121181405+Sarita-021@users.noreply.github.com>
2025-04-11 17:11:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -643,6 +643,7 @@ def test_email():
         return f'Error sending email: {e}'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
+    app.run(host="0.0.0.0", port=port, debug=True)  
 
 
